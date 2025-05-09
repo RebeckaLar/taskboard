cc1e0430213a277f04c67aea97e6272f6adf429c
Rebecka <rebecka.rl.larsson@gmail.com>
2025-05-09 12:47:39 +0100

    Not found page created and pages to admin links added

--- src/components/avatar-dropdown.jsx
+++ src/components/avatar-dropdown.jsx
@@ -49,7 +49,7 @@ export const AvatarDropdown = () => {
                         </Link>
                     </DropdownMenuItem>
 
-                    <DropdownMenuSeparator />
+                    <DropdownMenuSeparator className="md:hidden"/>
                    </> 
                 )
             }
