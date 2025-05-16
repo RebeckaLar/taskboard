e5045942253bb558e0379130e5faedbe7ef8d10a
Rebecka <rebecka.rl.larsson@gmail.com>
2025-05-16 16:26:03 +0100

    Drag and drop tasks possible. Sorting added

--- src/components/tasks/task-column.jsx
+++ src/components/tasks/task-column.jsx
@@ -2,67 +2,86 @@
 
 import { cn } from "@/lib/utils"
 import { TaskList } from "./task-list"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useTasks } from "@/context/tasksContext"
-// import { collection, doc, getDocs } from "firebase/firestore"
-// import { db } from "@/lib/firebase"
-
-// const TASKS = [
-//     {
-//         id: 1,
-//         title: "task 1"
-//     },
-//     {
-//         id: 2,
-//         title: "task 2"
-//     },
-//     {
-//         id: 3,
-//         title: "task 3"
-//     },
-//     {
-//         id: 4,
-//         title: "task 4"
-//     }
-// ]
+import { useAuth } from "@/context/authContext"
+import { Switch } from "../ui/switch"
+import { TaskProgress } from "./task-progress"
+import { TaskReorder } from "./task-reorder"
+import { useConfetti } from "@/context/confettiContext"
 
 export const TaskColumn = ({ user, date, className }) => {
 
-    const { getTasksByUserForDate, completeTask } = useTasks()
+    const [isReordering, setIsReordering, saveReorder] = useState(false)
+    const [localTasks, setLocalTasks] = useState([]) //fyll i med tasks när vi börjar sortera
 
+    //To avoid updating EVERY task, and only the actually moved tasks:
+    //Not using usestate
+    const movedTasks = useRef([])
+
+    const { getTasksByUserForDate, completeTask } = useTasks()
+    //Tasks from db
     const tasks = getTasksByUserForDate(user.uid, date)
 
     //Return non-completed tasks
     const notCompleted = tasks.filter(task => !task.completed)
 
+    const { isAdmin } = useAuth()
+    const { showConfetti } = useConfetti()
+
     const handleComplete = async (task) => {
         completeTask(task.id)
+        if(tasks.length > 0 && notCompleted.length === 1) {
+            showConfetti()
+        }
     }
 
-    // const [tasks, setTasks] = useState([])
-    //ALTERNATIVE:
-    // useEffect(() => {
-    //     const getTasks = async () => {
-    //         const querySnapshot = await getDocs(collection(db, "tasks"))
-    //         const data = []
-    //         querySnapshot.forEach(doc => {
-    //             data.push({
-    //             id: doc.id, 
-    //             ...doc.data() 
-    //             })
-    //         })
-    //         setTasks(data)
-    //     }
-    //     getTasks()
-    // }, [])
+    //Djup kopia av "tasks". Vill inte ha shallow obj som pekar på samma ref obj, utan helt ny kopia:
+    const startReorder = () => {
+        const deep = tasks
+        .filter(t => !t.completed) //Return  non-completed tasks
+        .map(t => ({ ...t }))
 
+        movedTasks.current = []
+        setLocalTasks(deep)
+    }
+
+    const handleCheckChange = (checked) => {
+        if(!checked) { //Om inte completed
+            //spara ordningen till databasen
+            const payload = movesTasks.current.filter(mt => {
+            const original = localTasks.find(t => t.id === mt.id)
+            return original && original.order !== mt.newOrder
+            })
+            if(payload.length < 0) {
+                saveReorder(localTasks, payload)
+            }
+        } else {
+            startReorder()
+        }
+        setIsReordering(checked)
+    }
 
   return (
     <div className={cn("bg-foreground/20 max-w-96 p-5 mx-auto rounded-xl flex flex-col", className)}>
-        {/* TaskProgress */}
-        {/* Admin switch */}
+        <TaskProgress total={tasks.length} user={user} completed={tasks.length - notCompleted.length} className="mb-5"/> 
+        {
+            isAdmin() && (
+                <div className="flex items-center justify-between mb-5">
+                    <span className="font-medium">Sortera</span>
+                    <Switch 
+                        checked={isReordering}
+                        onCheckedChange={handleCheckChange}
+                    />
+                </div>
+            )
+        }
         <div className="flex-1">
-            <TaskList tasks={notCompleted} handleComplete={handleComplete} />
+            {
+            isReordering
+                ? <TaskReorder tasks={localTasks} setTasks={setLocalTasks} movedTasks={movedTasks}/>
+                : <TaskList tasks={notCompleted} handleComplete={handleComplete} />
+            }
         </div>
         {/* admin? Add btn */}
     </div>
